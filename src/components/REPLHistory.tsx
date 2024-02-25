b82cd723e60200d5e391a842d0239123f0ce9ec5
“emonch03” <enno_moench@brown.edu>
2024-02-24 19:20:31 -0500

    feat: add User Story 1

--- src/components/REPLHistory.tsx
+++ src/components/REPLHistory.tsx
@@ -1,17 +1,18 @@
 import "../styles/main.css";
 
 interface REPLHistoryProps {
-  // TODO: Fill with some shared state tracking all the pushed commands
   history: string[];
 }
-export function REPLHistory(props: REPLHistoryProps) {
+export function REPLHistory({ history }: REPLHistoryProps) {
   return (
     <div className="repl-history">
-      {/* This is where command history will go */}
-      {/* TODO: To go through all the pushed commands... try the .map() function! */}
-      {props.history.map((command, index) => (
-        <p>{command}</p>
-      ))}
+      {history.map((entry, index) => {
+      
+        const parts = entry.split('\n').map((part, partIndex) => (
+          <div key={partIndex}>{part}</div>
+        ));
+        return <div key={index}>{parts}</div>;
+      })}
     </div>
   );
 }
