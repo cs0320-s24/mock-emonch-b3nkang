cb946212259749d51e10db932c5d78031fcde2bc
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2024-03-01 13:58:41 -0500

    test: add first e2e test implementation

--- tests/e2e/App.spec.ts
+++ tests/e2e/App.spec.ts
@@ -9,84 +9,127 @@ import { expect, test } from "@playwright/test";
  */
 
 // If you needed to do something before every test case...
-test.beforeEach(() => {
+test.beforeEach(async ({ page }) => {
   // ... you'd put it here.
-  // TODO: Is there something we need to do before every test case to avoid repeating code?
+  await page.goto("http://localhost:8000/");
 });
 
-/**
- * Don't worry about the "async" yet. We'll cover it in more detail
- * for the next sprint. For now, just think about "await" as something
- * you put before parts of your test that might take time to run,
- * like any interaction with the page.
- */
-test("on page load, i see a login button", async ({ page }) => {
-  // Notice: http, not https! Our front-end is not set up for HTTPs.
-  await page.goto("http://localhost:8000/");
+// test("on page load, i dont see the input box until login", async ({ page }) => {
+//   // Notice: http, not https! Our front-end is not set up for HTTPs.
+//   await page.goto("http://localhost:8000/");
+//   await expect(page.getByLabel("Sign Out")).not.toBeVisible();
+//   await expect(page.getByLabel("Command input")).not.toBeVisible();
+
+//   // click the login button
+//   await page.getByLabel("Login").click();
+//   await expect(page.getByLabel("Sign Out")).toBeVisible();
+//   await expect(page.getByLabel("Command input")).toBeVisible();
+// });
+
+test("on page load, i dont see the input box until pressing login", async ({
+  page,
+}) => {
   await expect(page.getByLabel("Login")).toBeVisible();
+  await expect(page.locator(".repl")).not.toBeVisible();
 });
 
-test("on page load, i dont see the input box until login", async ({ page }) => {
-  // Notice: http, not https! Our front-end is not set up for HTTPs.
-  await page.goto("http://localhost:8000/");
-  await expect(page.getByLabel("Sign Out")).not.toBeVisible();
-  await expect(page.getByLabel("Command input")).not.toBeVisible();
-
-  // click the login button
+test("after login, the repl is now visible", async ({ page }) => {
   await page.getByLabel("Login").click();
-  await expect(page.getByLabel("Sign Out")).toBeVisible();
-  await expect(page.getByLabel("Command input")).toBeVisible();
+  await expect(page.locator(".repl")).toBeVisible();
 });
 
-test("after I type into the input box, its text changes", async ({ page }) => {
-  // Step 1: Navigate to a URL
-  await page.goto("http://localhost:8000/");
+test("after login, i can switch modes between verbose and brief", async ({
+  page,
+}) => {
   await page.getByLabel("Login").click();
-
-  // Step 2: Interact with the page
-  // Locate the element you are looking for
-  await page.getByLabel("Command input").click();
-  await page.getByLabel("Command input").fill("Awesome command");
-
-  // Step 3: Assert something about the page
-  // Assertions are done by using the expect() function
-  const mock_input = `Awesome command`;
-  await expect(page.getByLabel("Command input")).toHaveValue(mock_input);
+  await page.getByLabel("Command input").fill("mode");
+  await page.getByRole("button", { name: "Submit Command" }).click();
+  await expect(page.getByText("Command: mode")).toBeVisible();
+  await expect(page.getByText("Output: Switched to verbose")).toBeVisible();
 });
 
-test("on page load, i see a button", async ({ page }) => {
-  // CHANGED
-  await page.goto("http://localhost:8000/");
+test("after login, i can load a CSV file and view data", async ({ page }) => {
   await page.getByLabel("Login").click();
-  await expect(
-    page.getByRole("button", { name: "Submitted 0 times" })
-  ).toBeVisible();
+  await page.getByLabel("Command input").fill("load_file path1.csv");
+  await page.getByRole("button", { name: "Submit Command" }).click();
+  // to do
 });
 
-test("after I click the button, its label increments", async ({ page }) => {
-  // CHANGED
-  await page.goto("http://localhost:8000/");
-  await page.getByLabel("Login").click();
-  await expect(
-    page.getByRole("button", { name: "Submitted 0 times" })
-  ).toBeVisible();
-  await page.getByRole("button", { name: "Submitted 0 times" }).click();
-  await expect(
-    page.getByRole("button", { name: "Submitted 1 times" })
-  ).toBeVisible();
-});
+// npx playwright codegen http://localhost:8000/
 
-test("after I click the button, my command gets pushed", async ({ page }) => {
-  // CHANGED
-  await page.goto("http://localhost:8000/");
-  await page.getByLabel("Login").click();
-  await page.getByLabel("Command input").fill("Awesome command");
-  await page.getByRole("button", { name: "Submitted 0 times" }).click();
+// /**
+//  * Don't worry about the "async" yet. We'll cover it in more detail
+//  * for the next sprint. For now, just think about "await" as something
+//  * you put before parts of your test that might take time to run,
+//  * like any interaction with the page.
+//  */
+// test("on page load, i see a login button", async ({ page }) => {
+//   // Notice: http, not https! Our front-end is not set up for HTTPs.
+//   await page.goto("http://localhost:8000/");
+//   await expect(page.getByLabel("Login")).toBeVisible();
+// });
 
-  // you can use page.evaulate to grab variable content from the page for more complex assertions
-  const firstChild = await page.evaluate(() => {
-    const history = document.querySelector(".repl-history");
-    return history?.children[0]?.textContent;
-  });
-  expect(firstChild).toEqual("Awesome command");
-});
+// test("on page load, i dont see the input box until login", async ({ page }) => {
+//   // Notice: http, not https! Our front-end is not set up for HTTPs.
+//   await page.goto("http://localhost:8000/");
+//   await expect(page.getByLabel("Sign Out")).not.toBeVisible();
+//   await expect(page.getByLabel("Command input")).not.toBeVisible();
+
+//   // click the login button
+//   await page.getByLabel("Login").click();
+//   await expect(page.getByLabel("Sign Out")).toBeVisible();
+//   await expect(page.getByLabel("Command input")).toBeVisible();
+// });
+
+// test("after I type into the input box, its text changes", async ({ page }) => {
+//   // Step 1: Navigate to a URL
+//   await page.goto("http://localhost:8000/");
+//   await page.getByLabel("Login").click();
+
+//   // Step 2: Interact with the page
+//   // Locate the element you are looking for
+//   await page.getByLabel("Command input").click();
+//   await page.getByLabel("Command input").fill("Awesome command");
+
+//   // Step 3: Assert something about the page
+//   // Assertions are done by using the expect() function
+//   const mock_input = `Awesome command`;
+//   await expect(page.getByLabel("Command input")).toHaveValue(mock_input);
+// });
+
+// test("on page load, i see a button", async ({ page }) => {
+//   // CHANGED
+//   await page.goto("http://localhost:8000/");
+//   await page.getByLabel("Login").click();
+//   await expect(
+//     page.getByRole("button", { name: "Submitted 0 times" })
+//   ).toBeVisible();
+// });
+
+// // test("after I click the button, its label increments", async ({ page }) => {
+// //   // CHANGED
+// //   await page.goto("http://localhost:8000/");
+// //   await page.getByLabel("Login").click();
+// //   await expect(
+// //     page.getByRole("button", { name: "Submitted 0 times" })
+// //   ).toBeVisible();
+// //   await page.getByRole("button", { name: "Submitted 0 times" }).click();
+// //   await expect(
+// //     page.getByRole("button", { name: "Submitted 1 times" })
+// //   ).toBeVisible();
+// // });
+
+// test("after I click the button, my command gets pushed", async ({ page }) => {
+//   // CHANGED
+//   await page.goto("http://localhost:8000/");
+//   await page.getByLabel("Login").click();
+//   await page.getByLabel("Command input").fill("Awesome command");
+//   // await page.getByRole("button", { name: "Submitted 0 times" }).click();
+
+//   // you can use page.evaulate to grab variable content from the page for more complex assertions
+//   const firstChild = await page.evaluate(() => {
+//     const history = document.querySelector(".repl-history");
+//     return history?.children[0]?.textContent;
+//   });
+//   expect(firstChild).toEqual("Awesome command");
+// });
